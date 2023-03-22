b76635e0d89b172f8bb0ffc01aa1a29a4bc96bca
Josep Roure Alcobé <roure@tecnocampus.cat>
2023-03-22 13:26:48 +0100

    completely removed restTemplate

--- front/src/main/java/cat/tecnocampus/front/application/ApplicationController.java
+++ front/src/main/java/cat/tecnocampus/front/application/ApplicationController.java
@@ -1,14 +1,10 @@
 package cat.tecnocampus.front.application;
 
 import cat.tecnocampus.front.domain.Product;
-//import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,19 +15,16 @@ import java.util.List;
 @Service
 public class ApplicationController {
 
-    private final RestTemplate restTemplate;
     private final ReactiveCircuitBreakerFactory circuitBreakerFactory;
 
     private final WebClient webClient;
 
     private String productServiceUrl;
 
-    public ApplicationController(RestTemplate restTemplate,
-                                 @Value("${app.product-service.host}") String productServiceHost,
+    public ApplicationController(@Value("${app.product-service.host}") String productServiceHost,
                                  ReactiveCircuitBreakerFactory circuitBreakerFactory,
                                  WebClient webClient)
     {
-        this.restTemplate = restTemplate;
         productServiceUrl = "http://" + productServiceHost + "/products";
         this.circuitBreakerFactory = circuitBreakerFactory;
         this.webClient = webClient;
@@ -42,7 +35,6 @@ public class ApplicationController {
                 .uri(productServiceUrl)
                 .body(Mono.just(product), Product.class)
                 .retrieve().bodyToMono(String.class).block();
-        //restTemplate.postForObject(productServiceUrl, product, String.class);
     }
 
     public List<Product> getProducts() {
